4fdc8d93b666aac3f2caa3fc8e4ab00017204e24
Mark Lilback <mark@lilback.com>
2016-08-11 14:34:13 -0400

    first commit with hadoop, hbase, rstudio

---
+++ hbase/start-pseudo-distributed.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+/opt/hbase/bin/hbase zookeeper > logzoo.log 2>&1 &
+/opt/hbase/bin/hbase regionserver start > logregion.log 2>&1 &
+
+/opt/hbase/bin/hbase thrift start &
+/opt/hbase/bin/hbase master start --localRegionServers=0
