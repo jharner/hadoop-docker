01022fd5ef5bfaf9dd04bedf82bac99c5bda160a
Mark Lilback <mark@lilback.com>
2016-08-16 17:46:54 -0400

    fixed env variables for hadoop streaming

--- hadoop/bootstrap.sh
+++ hadoop/bootstrap.sh
@@ -17,14 +17,24 @@ if [[ ! -d /hadoop-data/ ]]; then
 	exit 1
 fi
 
+CREATEDIR=0
 if [[ ! -d /hadoop-data/nn ]]; then
 	rm -rf /hadoop-data/dfs
 	/usr/local/hadoop/bin/hdfs namenode -format 
+	CREATEDIR=1
 fi
 
 service sshd start
 $HADOOP_PREFIX/sbin/start-dfs.sh
 $HADOOP_PREFIX/sbin/start-yarn.sh
+
+if [[ CREATEDIR ]]; then
+	sleep 5
+	echo "creating hdfs:/user/rstudio"
+	/usr/local/hadoop/bin/hdfs dfs -mkdir /user
+	/usr/local/hadoop/bin/hdfs dfs -mkdir /user/rstudio
+	/usr/local/hadoop/bin/hdfs dfs -chown rstudio:rstudio /user/rstudio
+fi
 
 if [[ $1 == "-d" ]]; then
   while true; do sleep 1000; done
