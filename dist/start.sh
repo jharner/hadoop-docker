8c3a539d9f4d540db5dc188a60e4319dcf7f0a7e
Mark Lilback <mark@lilback.com>
2016-08-29 16:11:38 -0400

    updated dist version to work with images from docker-hub

--- dist/start.sh
+++ dist/start.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
-if [[ ! -e pgdata ]]; then
-	mkdir pgdata
+if [[ "reset" -eq $1 ]]; then
+	rm -rf data/pg-data data/hadoop
 fi
 
-if [[ ! -e rstudio-data ]]; then
-	mkdir rstudio-data
+if [[ ! -e data ]]; then
+	mkdir data
 fi
 
-if [[ ! -e hadoop-data ]]; then
-	mkdir hadoop-data
+if [[ ! -e data/pg-data ]]; then
+	mkdir data/pg-data
+fi
+
+if [[ ! -e data/rstudio ]]; then
+	mkdir data/rstudio
+fi
+
+if [[ ! -e data/hadoop ]]; then
+	mkdir data/hadoop
 fi
 
 docker-compose up
